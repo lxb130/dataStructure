49401b5a4f62b84e0112e0c066880af0f212a89e
lxb130 <lxb1304@163.com>
2020-03-08 10:34:47 +0800

    双向链表crud实现

--- linklist/line.c
+++ linklist/line.c
@@ -46,6 +46,86 @@ void display(line *list)
     }
 }
 
+int getElem(line *list, int elem)
+{
+    line *head = list;
+    while (head)
+    {
+        if (head->data == elem)
+        {
+            printf("%d\n", head->data);
+        }
+
+        head = head->next;
+    }
+    return -1;
+}
+
+line *insertLine(line *head, int data, int add)
+{
+    //新建数据域为data的结点
+    line *temp = (line *)malloc(sizeof(line));
+    temp->data = data;
+    temp->prior = NULL;
+    temp->next = NULL;
+    //插入到链表头，要特殊考虑
+    if (add == 1)
+    {
+        temp->next = head;
+        head->prior = temp;
+        head = temp;
+    }
+    else
+    {
+        line *body = head;
+        //找到要插入位置的前一个结点
+        for (int i = 1; i < add - 1; i++)
+        {
+            body = body->next;
+        }
+        //判断条件为真，说明插入位置为链表尾
+        if (body->next == NULL)
+        {
+            body->next = temp;
+            temp->prior = body;
+        }
+        else
+        {
+            body->next->prior = temp;
+            temp->next = body->next;
+            body->next = temp;
+            temp->prior = body;
+        }
+    }
+    return head;
+}
+
+line *deleteLine(line *list, int data)
+{
+    line *head = list;
+    while (head)
+    {
+        if (head->data == data)
+        {
+            head->prior->next = head->next;
+            head->next->prior = head->prior;
+            free(head);
+        }
+        head = head->next;
+    }
+    return list;
+}
+
+line *updateElem(line *list, int pos, int newElem)
+{
+    line *head = list;
+    for (int i = 1; i < pos; i++)
+    {
+        head = head->next;
+    }
+    head->data = newElem;
+    return list;
+}
 int main()
 {
     //创建一个头指针
@@ -54,7 +134,14 @@ int main()
     head = initLine(head);
     //输出创建好的链表
     display(head);
-    //显示双链表的优点
-    printf("链表中第 4 个节点的直接前驱是：%d", head->next->next->next->prior->data);
+    // getElem(head, 2);
+    // head = insertLine(head, 8, 1)
+    // display(head);
+    // head = insertLine(head, 9, 5);
+    // display(head);
+    // head = deleteLine(head, 9);
+    // display(head);
+    head = updateElem(head, 4, 10);
+    display(head);
     return 0;
 }
